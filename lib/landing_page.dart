acf13a7c8ae347cc3f521ccac1295e5f8e49f525
rish07 <rishirajsinghchhabra@gmail.com>
2021-03-28 19:24:09 +0530

    Add circular progress indicator

--- lib/landing_page.dart
+++ lib/landing_page.dart
@@ -11,8 +11,9 @@ class LandingPage extends StatefulWidget {
 }
 
 class _LandingPageState extends State<LandingPage> {
-  WeatherModel weather;
-  static String location = "Bhopal";
+  WeatherModel weather = WeatherModel();
+  static String location = "Jaipur";
+  bool _isLoading = true;
   var baseUrl =
       "https://api.openweathermap.org/data/2.5/weather?q=$location&appid=c990d19da8b95bb4cd3d357bcebfc5d0&units=metric";
 
@@ -22,6 +23,7 @@ class _LandingPageState extends State<LandingPage> {
       weather = WeatherModel.fromJson(
         json.decode(response.body),
       );
+      _isLoading = false;
     });
   }
 
@@ -29,64 +31,176 @@ class _LandingPageState extends State<LandingPage> {
   void initState() {
     // TODO: implement initState
     super.initState();
+
     getWeather();
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Stack(children: [
-        Container(
-          child: Image.asset(
-            weather.weather[0].main == "Clear"
-                ? "assets/sunny.jpg"
-                : (weather.weather[0].main == "Rain")
-                    ? "assets/rainy.jpg"
-                    : "assets/cloudy.jpg",
-            fit: BoxFit.cover,
-            height: double.infinity,
-            width: double.infinity,
-          ),
-        ),
-        Container(
-          decoration: BoxDecoration(color: Colors.black38),
-        ),
-        Container(
-          padding: EdgeInsets.all(20),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              Expanded(
+      body: _isLoading
+          ? Center(
+              child: CircularProgressIndicator(),
+            )
+          : Stack(children: [
+              Container(
+                child: Image.asset(
+                  weather.weather[0].main == "Clear"
+                      ? "assets/sunny.jpg"
+                      : (weather.weather[0].main == "Rain")
+                          ? "assets/rainy.jpg"
+                          : "assets/cloudy.jpeg",
+                  fit: BoxFit.cover,
+                  height: double.infinity,
+                  width: double.infinity,
+                ),
+              ),
+              Container(
+                decoration: BoxDecoration(color: Colors.black38),
+              ),
+              Container(
+                padding: EdgeInsets.all(20),
                 child: Column(
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
                   crossAxisAlignment: CrossAxisAlignment.center,
                   children: [
-                    Text(
-                      weather.name,
-                      style: GoogleFonts.lato(
-                        fontSize: 35,
-                        fontWeight: FontWeight.bold,
-                        color: Colors.white,
+                    Expanded(
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        children: [
+                          Text(
+                            weather.name,
+                            style: GoogleFonts.lato(
+                              fontSize: 35,
+                              fontWeight: FontWeight.bold,
+                              color: Colors.white,
+                            ),
+                          ),
+                          Column(
+                            crossAxisAlignment: CrossAxisAlignment.center,
+                            children: [
+                              Text(
+                                weather.main.temp.toString(),
+                                style: GoogleFonts.lato(
+                                  fontSize: 85,
+                                  fontWeight: FontWeight.w300,
+                                  color: Colors.white,
+                                ),
+                              ),
+                              Text(
+                                weather.weather[0].description,
+                                style: GoogleFonts.lato(
+                                  fontSize: 25,
+                                  fontWeight: FontWeight.w500,
+                                  color: Colors.white,
+                                ),
+                              ),
+                            ],
+                          ),
+                        ],
                       ),
                     ),
                     Column(
-                      crossAxisAlignment: CrossAxisAlignment.center,
                       children: [
-                        Text(
-                          weather.main.temp.toString(),
-                          style: GoogleFonts.lato(
-                            fontSize: 85,
-                            fontWeight: FontWeight.w300,
-                            color: Colors.white,
+                        Container(
+                          margin: EdgeInsets.symmetric(vertical: 40),
+                          decoration: BoxDecoration(
+                            border: Border.all(
+                              color: Colors.white30,
+                            ),
                           ),
                         ),
-                        Text(
-                          weather.weather[0].description,
-                          style: GoogleFonts.lato(
-                            fontSize: 25,
-                            fontWeight: FontWeight.w500,
-                            color: Colors.white,
+                        Padding(
+                          padding: const EdgeInsets.all(16),
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              Column(
+                                children: [
+                                  Text(
+                                    'Wind',
+                                    style: GoogleFonts.lato(
+                                      fontSize: 14,
+                                      fontWeight: FontWeight.bold,
+                                      color: Colors.white,
+                                    ),
+                                  ),
+                                  Text(
+                                    weather.wind.speed.toString(),
+                                    style: GoogleFonts.lato(
+                                      fontSize: 24,
+                                      fontWeight: FontWeight.bold,
+                                      color: Colors.white,
+                                    ),
+                                  ),
+                                  Text(
+                                    'km/h',
+                                    style: GoogleFonts.lato(
+                                      fontSize: 14,
+                                      fontWeight: FontWeight.bold,
+                                      color: Colors.white,
+                                    ),
+                                  ),
+                                ],
+                              ),
+                              Column(
+                                children: [
+                                  Text(
+                                    'Pressure',
+                                    style: GoogleFonts.lato(
+                                      fontSize: 14,
+                                      fontWeight: FontWeight.bold,
+                                      color: Colors.white,
+                                    ),
+                                  ),
+                                  Text(
+                                    weather.main.pressure.toString(),
+                                    style: GoogleFonts.lato(
+                                      fontSize: 24,
+                                      fontWeight: FontWeight.bold,
+                                      color: Colors.white,
+                                    ),
+                                  ),
+                                  Text(
+                                    'hPa',
+                                    style: GoogleFonts.lato(
+                                      fontSize: 14,
+                                      fontWeight: FontWeight.bold,
+                                      color: Colors.white,
+                                    ),
+                                  ),
+                                ],
+                              ),
+                              Column(
+                                children: [
+                                  Text(
+                                    'Humidity',
+                                    style: GoogleFonts.lato(
+                                      fontSize: 14,
+                                      fontWeight: FontWeight.bold,
+                                      color: Colors.white,
+                                    ),
+                                  ),
+                                  Text(
+                                    weather.main.humidity.toString(),
+                                    style: GoogleFonts.lato(
+                                      fontSize: 24,
+                                      fontWeight: FontWeight.bold,
+                                      color: Colors.white,
+                                    ),
+                                  ),
+                                  Text(
+                                    '%',
+                                    style: GoogleFonts.lato(
+                                      fontSize: 14,
+                                      fontWeight: FontWeight.bold,
+                                      color: Colors.white,
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ],
                           ),
                         ),
                       ],
@@ -94,114 +208,7 @@ class _LandingPageState extends State<LandingPage> {
                   ],
                 ),
               ),
-              Column(
-                children: [
-                  Container(
-                    margin: EdgeInsets.symmetric(vertical: 40),
-                    decoration: BoxDecoration(
-                      border: Border.all(
-                        color: Colors.white30,
-                      ),
-                    ),
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.all(16),
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        Column(
-                          children: [
-                            Text(
-                              'Wind',
-                              style: GoogleFonts.lato(
-                                fontSize: 14,
-                                fontWeight: FontWeight.bold,
-                                color: Colors.white,
-                              ),
-                            ),
-                            Text(
-                              weather.wind.speed.toString(),
-                              style: GoogleFonts.lato(
-                                fontSize: 24,
-                                fontWeight: FontWeight.bold,
-                                color: Colors.white,
-                              ),
-                            ),
-                            Text(
-                              'km/h',
-                              style: GoogleFonts.lato(
-                                fontSize: 14,
-                                fontWeight: FontWeight.bold,
-                                color: Colors.white,
-                              ),
-                            ),
-                          ],
-                        ),
-                        Column(
-                          children: [
-                            Text(
-                              'Pressure',
-                              style: GoogleFonts.lato(
-                                fontSize: 14,
-                                fontWeight: FontWeight.bold,
-                                color: Colors.white,
-                              ),
-                            ),
-                            Text(
-                              weather.main.pressure.toString(),
-                              style: GoogleFonts.lato(
-                                fontSize: 24,
-                                fontWeight: FontWeight.bold,
-                                color: Colors.white,
-                              ),
-                            ),
-                            Text(
-                              'hPa',
-                              style: GoogleFonts.lato(
-                                fontSize: 14,
-                                fontWeight: FontWeight.bold,
-                                color: Colors.white,
-                              ),
-                            ),
-                          ],
-                        ),
-                        Column(
-                          children: [
-                            Text(
-                              'Humidity',
-                              style: GoogleFonts.lato(
-                                fontSize: 14,
-                                fontWeight: FontWeight.bold,
-                                color: Colors.white,
-                              ),
-                            ),
-                            Text(
-                              weather.main.humidity.toString(),
-                              style: GoogleFonts.lato(
-                                fontSize: 24,
-                                fontWeight: FontWeight.bold,
-                                color: Colors.white,
-                              ),
-                            ),
-                            Text(
-                              '%',
-                              style: GoogleFonts.lato(
-                                fontSize: 14,
-                                fontWeight: FontWeight.bold,
-                                color: Colors.white,
-                              ),
-                            ),
-                          ],
-                        ),
-                      ],
-                    ),
-                  ),
-                ],
-              ),
-            ],
-          ),
-        ),
-      ]),
+            ]),
     );
   }
 }
